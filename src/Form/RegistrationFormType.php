31d23913ac14a63f37a941862a0e7dd87a20b637
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-09 15:26:22 +0100

    feat : add HomeController

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,6 +22,15 @@ class RegistrationFormType extends AbstractType
             ->add('email', EmailType::class,[
                 'label' =>'Saisir votre email :'
             ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Saisir votre Nom :',
+                'required'=> false
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Saisir votre Prénom :',
+                'required'=> false
+            ])
+            
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
